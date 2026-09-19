b227347a94938cced4ede278f36444581ca3c99d
Oleh Bohatyi <olehbohatyi@gmail.com>
2026-09-19 18:29:23 +0200

    refactor comments

--- problems/easy/0001_TwoSum.scala
+++ problems/easy/0001_TwoSum.scala
@@ -5,10 +5,10 @@ import scala.util.boundary, boundary.break
 
 //  1. Two Sum
 //
-//  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-//
-//  You may assume that each input would have exactly one solution, and you may not use the same element twice.
-//
+//  Given an array of integers nums and an integer target,
+//  return indices of the two numbers such that they add up to target.
+//  You may assume that each input would have exactly one solution,
+//  and you may not use the same element twice.
 //  You can return the answer in any order.
 //
 //  Example 1:
@@ -25,7 +25,6 @@ import scala.util.boundary, boundary.break
 //  Output: [0,1]
 //
 //  Constraints:
-//
 //  2 <= nums.length <= 104
 //  -109 <= nums[i] <= 109
 //  -109 <= target <= 109
--- problems/medium/0002_AddTwoNumbers.scala
+++ problems/medium/0002_AddTwoNumbers.scala
@@ -1,12 +1,13 @@
 import scala.annotation.tailrec
+
 //  2. Add Two Numbers
 //
-//  You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
-//
+//  You are given two non-empty linked lists representing two non-negative integers.
+//  The digits are stored in reverse order, and each of their nodes contains a single digit.
+//  Add the two numbers and return the sum as a linked list.
 //  You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 //
 //  Example 1:
-//
 //  Input: l1 = [2,4,3], l2 = [5,6,4]
 //  Output: [7,0,8]
 //  Explanation: 342 + 465 = 807.
@@ -16,12 +17,10 @@ import scala.annotation.tailrec
 //  Output: [0]
 //
 //  Example 3:
-//
 //  Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 //  Output: [8,9,9,9,0,0,0,1]
 //
 //  Constraints:
-//
 //  The number of nodes in each linked list is in the range [1, 100].
 //  0 <= Node.val <= 9
 //  It is guaranteed that the list represents a number that does not have leading zeros.
@@ -108,6 +107,7 @@ def twoNumbersFPScala3(l1: ListNode, l2: ListNode): ListNode =
   loop(l1, l2, 0)
 
 @main def addTwoNumbers(): Unit =
+
   val ln11 = ListNode(2, ListNode(4, ListNode(3, null)))
   val ln12 = ListNode(5, ListNode(6, ListNode(4, null)))
   val out1 = ListNode(7, ListNode(0, ListNode(8, null)))
