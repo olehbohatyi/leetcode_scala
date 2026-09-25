264cce018c45d7d94f27096423a1996e972035bc
Oleh Bohatyi <olehbohatyi@gmail.com>
2026-09-25 20:42:46 +0200

    feat(palindrome-number): add multiple implementations

--- problems/easy/0009_PalindromeNumber.scala
+++ problems/easy/0009_PalindromeNumber.scala
@@ -27,6 +27,8 @@
 
 // Follow up: Could you solve it without converting the integer to a string?
 
+import scala.annotation.tailrec
+
 def palindromeNumberHalfReverse(x: Int): Boolean =
   if x < 0 || (x % 10 == 0 && x != 0) then false
   else
@@ -37,6 +39,48 @@ def palindromeNumberHalfReverse(x: Int): Boolean =
       rem /= 10
     rem == rev || rem == rev / 10
 
+def palindromeNumberFullReverse(x: Int): Boolean =
+  if x < 0 then false
+  else
+    var rem = x
+    var rev = 0L
+    while rem > 0 do
+      rev = rev * 10 + rem % 10
+      rem /= 10
+    rev == x
+
+def palindromeNumberCompareDigits(x: Int): Boolean =
+  if x < 0 then false
+  else
+    var rem = x
+    var divisor = 1
+    while rem / divisor >= 10 do divisor *= 10
+    while rem > 0 do
+      if rem / divisor != rem % 10 then return false
+      rem = (rem % divisor) / 10
+      divisor /= 100
+    true
+
+def palindromeNumberTailrec(x: Int): Boolean =
+  if x < 0 || (x % 10 == 0 && x != 0) then false
+  else
+    @tailrec
+    def loop(remaining: Int, reversedHalf: Int): Boolean =
+      if remaining <= reversedHalf then
+        remaining == reversedHalf || remaining == reversedHalf / 10
+      else loop(remaining / 10, reversedHalf * 10 + remaining % 10)
+
+    loop(x, 0)
+
+def palindromeNumberFold(x: Int): Boolean =
+  if x < 0 then false
+  else
+    val digits = List.unfold(x): remaining =>
+      if remaining == 0 then None
+      else Some((remaining % 10, remaining / 10))
+    digits.foldLeft(List.empty[Int])((reversed, digit) => digit :: reversed) == digits
+
+
 @main def palindromeNumber(): Unit =
 
   val data = List(
@@ -45,11 +89,18 @@ def palindromeNumberHalfReverse(x: Int): Boolean =
     10   -> false,
     0    -> true,
     1221 -> true,
-    1231 -> false
+    1231 -> false,
+    12321 -> true,
+    2147447412 -> true,
+    2147483647 -> false
   )
 
   val impl = List(
-    palindromeNumberHalfReverse
+    palindromeNumberHalfReverse,
+    palindromeNumberFullReverse,
+    palindromeNumberCompareDigits,
+    palindromeNumberTailrec,
+    palindromeNumberFold
   )
 
   for
